74ecfbf28ac1971b153e8a2324c0a1bef9610875
Anas <anasginolt@gmail.com>
2024-10-04 00:00:13 +0530

    intial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcemet'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
+      <Announcement />
       <Navbar/>
+      <Slider/>
+      <Categories/>
+      <Products/>
+      <Newsletter/>
+      <Footer/>
     </div>
   )
 }
