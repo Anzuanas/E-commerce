26fd96612fba2fe6803b9f762df6db62d3bf6f4e
Anas <anasginolt@gmail.com>
2024-10-04 23:51:05 +0530

    commit of friday

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Announcement from '../components/Announcemet'
+import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
 import Products from '../components/Products'
