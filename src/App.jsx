ebac1c1aad7f8c136c0eedb0361ad76c6216d856
Anas <anasginolt@gmail.com>
2024-10-10 16:28:43 +0530

    Login page responsive completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return <Login/>;
 };
 
-export default App;
+export default App;   
